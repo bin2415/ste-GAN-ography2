c4ffd55cde794c0670ebcc9a0d38be5cefd35a35
pang bin <pang bin>
2017-04-05 11:27:55 +0800

    模型

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ import matplotlib.pyplot as plt
 #sys.setrecursionlimit(1000)
 
 flags = tf.app.flags
-flags.DEFINE_float("alphaA", 0.6, "alphaA的值")
-flags.DEFINE_float("alphaB", 0.2, "alphaB的值")
-flags.DEFINE_float("alphaC", 0.2, "alphaC的值")
+flags.DEFINE_float("alphaA", 0.8, "alphaA的值")
+flags.DEFINE_float("alphaB", 0.4, "alphaB的值")
+flags.DEFINE_float("alphaC", -0.2, "alphaC的值")
 flags.DEFINE_float("learning_rate", 0.0008, "学习速率")
 flags.DEFINE_string("pic_dict", "./pictures", "存放的图片的位置")
 flags.DEFINE_string("save_pic_dict", "./savedPictures", "保存的图片位置")
