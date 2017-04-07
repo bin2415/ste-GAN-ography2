63395f588949d477db9580ce55fc139093ca23b3
pang bin <pang bin>
2017-04-07 20:29:28 +0800

    参数2

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ import matplotlib.pyplot as plt
 
 flags = tf.app.flags
 flags.DEFINE_float("alphaA", 0.8, "alphaA的值")
-flags.DEFINE_float("alphaB", 0.6, "alphaB的值")
-flags.DEFINE_float("alphaC", -0.4, "alphaC的值")
+flags.DEFINE_float("alphaB", 0.4, "alphaB的值")
+flags.DEFINE_float("alphaC", -0.2, "alphaC的值")
 flags.DEFINE_float("learning_rate", 0.0008, "学习速率")
 flags.DEFINE_string("pic_dict", "./pictures", "存放的图片的位置")
 flags.DEFINE_string("save_pic_dict", "./savedPictures", "保存的图片位置")
