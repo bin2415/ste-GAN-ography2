f8c458fc227b51556eeea89e6648e37e44f908d9
pang bin <pang bin>
2017-04-09 10:30:20 +0800

    eve not change

--- model.py
+++ model.py
@@ -243,7 +243,7 @@ class Model:
 
 ### Eve的网络结构
     def discriminator_stego_nn(self, img, batch_size, name):
-        eve_input = self.image_processing_layer(img)
+        #eve_input = self.image_processing_layer(img)
         eve_conv1 = convolution2d(img, 64, kernel_size = [5, 5], stride = [2,2],
         activation_fn= tf.nn.relu, normalizer_fn = BatchNorm, scope = 'eve/' + name + '/conv1')
 
