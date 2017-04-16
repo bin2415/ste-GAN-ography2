2f49c9b7e3bc339444521961ab684d8820b7af82
pang bin <pang bin>
2017-04-16 19:46:58 +0800

    比例增加

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import sys
 #alphaA 0.8 alphaB 0.4 alphaC -0.2
 #alphaA 1.2 alphaB 0.6 alphaC -0.2 alice误差60，bob误差0.5
 flags = tf.app.flags
-flags.DEFINE_float("alphaA", 1.2, "alphaA的值")
+flags.DEFINE_float("alphaA", 2.4, "alphaA的值")
 flags.DEFINE_float("alphaB", 0.6, "alphaB的值")
 flags.DEFINE_float("alphaC", -0.2, "alphaC的值")
 flags.DEFINE_float("learning_rate", 0.0008, "学习速率")
